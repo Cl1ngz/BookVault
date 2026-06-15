ddff9456323f2bd2a7328f0df18c28ece8218e09
bakerinho <dbaker2137@gmail.com>
2026-06-15 17:19:09 +0200

    Added auth to swagger

--- backend/src/main/java/com/bookvault/library/config/OpenApiConfig.java
+++ backend/src/main/java/com/bookvault/library/config/OpenApiConfig.java
@@ -1,7 +1,10 @@
 package com.bookvault.library.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,11 +20,24 @@ public class OpenApiConfig {
                 .url("http://localhost:8080")
                 .description("Local development server");
 
+        // Nazwa naszego schematu autoryzacji
+        final String securitySchemeName = "bearerAuth";
+
         return new OpenAPI()
                 .info(new Info()
                         .title("BookVault API")
                         .version("1.0")
                         .description("REST API for the BookVault library management system."))
-                .servers(List.of(devServer));
+                .servers(List.of(devServer))
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName,
+                                new SecurityScheme()
+                                        .name(securitySchemeName)
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                        )
+                );
     }
 }
