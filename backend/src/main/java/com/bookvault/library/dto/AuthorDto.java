f1009bbb6d426affd619dfc0d9688058281fc5e0
Cl1ngz <ByteIgma@gmail.com>
2026-06-18 14:31:20 +0200

    fixing last errors

--- backend/src/main/java/com/bookvault/library/dto/AuthorDto.java
+++ backend/src/main/java/com/bookvault/library/dto/AuthorDto.java
@@ -2,7 +2,6 @@ package com.bookvault.library.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
--- init-db/12_logika.sql
+++ init-db/12_logika.sql
@@ -4,7 +4,7 @@ DROP FUNCTION IF EXISTS biblioteka.pobierz_srednia_ocena(integer);
 CREATE FUNCTION biblioteka.pobierz_srednia_ocena(p_id_ksiazki INT)
 RETURNS DOUBLE PRECISION AS $$
 BEGIN
-    RETURN (SELECT ROUND(AVG(ocena)::numeric, 2) FROM recenzje WHERE id_ksiazki = p_id_ksiazki);
+    RETURN (SELECT ROUND(AVG(ocena)::numeric, 2) FROM biblioteka.recenzje WHERE id_ksiazki = p_id_ksiazki);
 END;
 $$ LANGUAGE plpgsql;
 
